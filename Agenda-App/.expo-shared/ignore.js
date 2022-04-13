162a5c2b9e344cc218a89c775f50829b713faa56
Muhammad Farisi Zatwara Putra Unyi <82500240+WussyCats@users.noreply.github.com>
2022-04-13 22:19:58 +0700

    Submit Source Code

---
+++ Agenda-App/.expo-shared/ignore.js
@@ -0,0 +1,61 @@
+/*
+let string =prompt("insert a "kamu" than "ke" than "tahu" than "an" ");
+try { 
+    if(isNaN(string)) throw "Kamu ketahuan (☉｡☉)!"
+
+    else if (string > nyontek) throw "Kenapa kamu clone punya aku ಠ︵ಠ ?!"
+
+    else if (string <= 30) throw "Aku sedih (ب_ب)" 
+    }
+    catch(e) {alert(e)}
+}
+
+Nama    : Muhammad Farisi Zatwara Putra Unyi
+NIM     : 119140201
+Kelas   : PAM RB
+
+let string =prompt("insert a "kamu" than "ke" than "tahu" than "an" ");
+try { 
+    if(isNaN(string)) throw "Kamu ketahuan (☉｡☉)!"
+
+    else if (string > nyontek) throw "Kenapa kamu clone punya aku ಠ︵ಠ ?!"
+
+    else if (string <= 30) throw "Aku sedih (ب_ب)" 
+    }
+    catch(e) {alert(e)}
+}
+
+Nama    : Muhammad Farisi Zatwara Putra Unyi
+NIM     : 119140201
+Kelas   : PAM RB
+
+let string =prompt("insert a "kamu" than "ke" than "tahu" than "an" ");
+try { 
+    if(isNaN(string)) throw "Kamu ketahuan (☉｡☉)!"
+
+    else if (string > nyontek) throw "Kenapa kamu clone punya aku ಠ︵ಠ ?!"
+
+    else if (string <= 30) throw "Aku sedih (ب_ب)" 
+    }
+    catch(e) {alert(e)}
+}
+
+Nama    : Muhammad Farisi Zatwara Putra Unyi
+NIM     : 119140201
+Kelas   : PAM RB
+
+let string =prompt("insert a "kamu" than "ke" than "tahu" than "an" ");
+try { 
+    if(isNaN(string)) throw "Kamu ketahuan (☉｡☉)!"
+
+    else if (string > nyontek) throw "Kenapa kamu clone punya aku ಠ︵ಠ ?!"
+
+    else if (string <= 30) throw "Aku sedih (ب_ب)" 
+    }
+    catch(e) {alert(e)}
+}
+
+Nama    : Muhammad Farisi Zatwara Putra Unyi
+NIM     : 119140201
+Kelas   : PAM RB
+*/ 
